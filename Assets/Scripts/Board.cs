4c3b8ef7225692aea5a2ee40f54e87da525e1586
Vladyslav Yakubenko <vladikyakubenko@gmail.com>
2022-12-06 15:38:33 +0200

    Making the pieces slide

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,15 +1,19 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Board : MonoBehaviour
 {
     public int x_widht;
     public int y_height;
+    public int offSet;
     public GameObject tilePrefarb;
     public GameObject[] candies;
     public Tile[,] allTiles;
     public GameObject[,] allCandies;
-    public Camera cam;
 
+    public Camera cam;
+    private int candyToUse;
 
     void Start()
     {
@@ -24,12 +28,20 @@ public class Board : MonoBehaviour
         {
             for (int j = 0; j < y_height; j++)
             {
-                Vector2 spawnPosition = new Vector2(i, j);
-
+                Vector2 spawnPosition = new Vector2(i, j + offSet);
                 GameObject tile = SpawnTile(spawnPosition);
                 GameObject candy = SpawnCandy(spawnPosition);
-
                 allCandies[i, j] = candy;
+                int maxIterations = 0;
+                while(MatchesAt(i,j, candies[candyToUse]) && maxIterations < 100)
+                {
+                    candyToUse = Random.Range(0,candies.Length);
+                    maxIterations++;
+                    Debug.Log(maxIterations);
+                }
+                maxIterations = 0;
+                candy.GetComponent<Candy>().row = j;
+                candy.GetComponent<Candy>().column = i;
             }
         }
     }
@@ -45,11 +57,136 @@ public class Board : MonoBehaviour
     private GameObject SpawnCandy(Vector2 candyPosition)
     {
         int candyToUse = Random.Range(0, candies.Length);
-
         GameObject candy = Instantiate(candies[candyToUse], candyPosition, Quaternion.identity);
         candy.transform.SetParent(transform);
         candy.GetComponent<Candy>().Init(cam, this);
         candy.name = $"(Candy {candyPosition.x}, {candyPosition.y} )";
         return candy;
+    }
+    private bool MatchesAt( int column, int row, GameObject piece)
+    {
+        if(column > 1 && row > 1) 
+        {
+            if (allCandies[column -1, row].tag == piece.tag && allCandies[column -2, row].tag == piece.tag)
+            {
+                return true;
+            }
+            if (allCandies[column, row -1].tag == piece.tag && allCandies[column, row -2].tag == piece.tag)
+            {
+                return true;
+            }
+        }
+        else if (column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (allCandies[column, row -1].tag == piece.tag && allCandies[column, row -2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allCandies[column -1, row].tag == piece.tag && allCandies[column -2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private void DestroyMatchesAt(int column, int row)
+    {
+        if(allCandies[column, row].GetComponent<Candy>().isMatched)
+        {
+            Destroy(allCandies[column, row]);
+            allCandies[column, row] = null;
+        }
+    }
+
+    public void DestroyMatches()
+    {
+        for (int i = 0; i < x_widht; i++)
+        {
+            for (int j = 0;  j< y_height; j++)
+            {
+                if (allCandies[i, j] != null)
+                {
+                    DestroyMatchesAt(i, j);
+                }
+            }
+        }
+        StartCoroutine(DecreaseRowCo());
+    }
+
+    private IEnumerator DecreaseRowCo()
+    {
+        int nullCount = 0;
+        for (int i = 0; i < x_widht; i++)
+        {
+            for (int j = 0; j < y_height; j++)
+            {
+                if (allCandies[i,j] == null)
+                {
+                    nullCount++;
+                }
+                else if(nullCount > 0)
+                {
+                    allCandies[i, j].GetComponent<Candy>().row -= nullCount;
+                    allCandies[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillBoardCo());
+    } 
+    private void RefillBoard()
+    {
+        for (int i = 0; i < x_widht; i++)
+        {
+            for (int j = 0; j < y_height; j++)
+            {
+                if (allCandies[i,j] == null)
+                {
+                    Vector2 tempPosition = new Vector2(i, j + offSet);
+                    int dotToUse = Random.Range(0, candies.Length);
+                    GameObject piece = Instantiate(candies[candyToUse], tempPosition, Quaternion.identity);
+                    allCandies[i, j] = piece;
+                    piece.GetComponent<Candy>().row = j;
+                    piece.GetComponent<Candy>().column = i;
+                }
+            }
+        }
+    }
+
+    private bool MatchesOnBoard()
+    {
+        for (int i = 0; i < x_widht; i++)
+        {
+            for(int j = 0; j < y_height; i++)
+            {
+                if (allCandies[i,j]!= null)
+                {
+                    if (allCandies[i, j].GetComponent<Candy>().isMatched)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator FillBoardCo()
+    {
+        RefillBoard();
+        yield return new WaitForSeconds(.5f);
+
+        while (MatchesOnBoard())
+        {
+            yield return new WaitForSeconds(.5f);
+            DestroyMatches();
+        }
     }
 }
--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -1,42 +1,59 @@
+using System.Collections;
 using UnityEngine;
 
 public class Candy : MonoBehaviour
-
 {
+    [Header("Board Variables")]
     public int column;
     public int row;
+    public int previousColumn;
+    public int previousRow;
     public int targetX;
     public int targetY;
+    public bool isMatched = false;
+
     private Board board;
     private GameObject otherCandy;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
     private Vector2 tempPosition;
     public float swipeAngle = 0;
-
-    private Camera _cam;
+    public float swipeResist = 1f;
+    private Camera _cam;    
 
     public void Init(Camera cam, Board board)
     {
         _cam = cam;
-
         this.board = board;
-        targetX = (int)transform.position.x;
-        targetY = (int)transform.position.y;
-        row = targetY;
-        column = targetX;
+        //targetX = (int)transform.position.x;
+        //targetY = (int)transform.position.y;
+        //row = targetY;
+        //column = targetX;
+        //previousRow = row;
+        //previousColumn = column;
     }
 
     void Update()
     {
+        FindMetches();
+        if (isMatched)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(1f, 1f, 1f, .2f);
+        }
+
         targetX = column;
         targetY = row;
         if (Mathf.Abs(targetX - transform.position.x) > .1)
         {
             //MOve Towards the target
             tempPosition = new Vector2(targetX, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
-        }
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .6f);
+            if (board.allCandies[column,row] != this.gameObject)
+            {
+                board.allCandies[column, row] = this.gameObject;
+            }
+        }       
         else
         {
             //Directly set the position
@@ -46,23 +63,46 @@ public class Candy : MonoBehaviour
         }
         if (Mathf.Abs(targetY - transform.position.y) > .1)
         {
-            //MOve Towards the target
+            //Move Towards the target
             tempPosition = new Vector2(transform.position.x, targetY);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .6f);
+            if (board.allCandies[column, row] != this.gameObject)
+            {
+                board.allCandies[column, row] = this.gameObject;
+            }
         }
         else
         {
             //Directly set the position
             tempPosition = new Vector2(transform.position.x, targetY);
-            transform.position = tempPosition;
-            board.allCandies[column, row] = this.gameObject;
+            transform.position = tempPosition;           
         }
+    }
+
+    public IEnumerator CheckMoveCo()
+    {
+        yield return new WaitForSeconds(.5f);
+        if(otherCandy != null)
+        {
+            if(!isMatched && !otherCandy.GetComponent<Candy>().isMatched)
+            {
+                otherCandy.GetComponent<Candy>().row = row;
+                otherCandy.GetComponent<Candy>().column = column;
+                row = previousRow;
+                column = previousColumn;
+            }
+            else
+            {
+                board.DestroyMatches();
+            }
+            otherCandy = null;
+        }       
     }
 
     private void OnMouseDown()
     {
         firstTouchPosition = _cam.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(firstTouchPosition);
+        //Debug.Log(firstTouchPosition);
     }
 
     private void OnMouseUp()
@@ -73,46 +113,83 @@ public class Candy : MonoBehaviour
 
     void CalculateAngle()
     {
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        Debug.Log(swipeAngle);
-        MovePieces();
-
+        if (Mathf.Abs(finalTouchPosition.y - firstTouchPosition.y) > swipeResist || Mathf.Abs(finalTouchPosition.x - firstTouchPosition.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+            MovePieces();
+        }        
     }
 
     void MovePieces()
     {
-
-        if(swipeAngle > -45 && swipeAngle <= 45 && column < board.x_widht)
+        if(swipeAngle > -45 && swipeAngle <= 45 && column < board.x_widht-1)
         {
             //Right Swipe
             otherCandy = board.allCandies[column + 1, row];
+            previousRow = row;
+            previousColumn = column;
             otherCandy.GetComponent<Candy>().column -= 1;
             column += 1;
-
         } 
-        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.y_height)
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.y_height-1)
         {
             //Up Swipe
             otherCandy = board.allCandies[column, row + 1];
+            previousRow = row;
+            previousColumn = column;
             otherCandy.GetComponent<Candy>().row -= 1;
             row += 1;
-
         }
         else if (swipeAngle > 135 ||  swipeAngle <= -135 && column > 0)
         {
             //Left Swipe
             otherCandy = board.allCandies[column - 1, row];
+            previousRow = row;
+            previousColumn = column;
             otherCandy.GetComponent<Candy>().column +=1;
             column -= 1;
-
         } 
         else if (swipeAngle < -45 && swipeAngle >= -135 && row >0)
         {
             //Down Swipe
             otherCandy = board.allCandies[column, row - 1];
-            otherCandy.GetComponent<Candy>().column +=1;
-            column -= 1;
+            previousRow = row;
+            previousColumn = column;
+            otherCandy.GetComponent<Candy>().row +=1;
+            row -= 1;
+        }
+        StartCoroutine(CheckMoveCo());
+    }
 
-        } 
+    void FindMetches()
+    {
+        if (column > 0 && column < board.x_widht - 1)
+        {
+            GameObject leftCandy1 = board.allCandies[column - 1, row];
+            GameObject rightCandy1 = board.allCandies[column + 1, row];
+            if(leftCandy1 !=null & rightCandy1 !=null ) 
+            {
+                if (leftCandy1.tag == this.gameObject.tag && rightCandy1.tag == this.gameObject.tag)
+                {
+                    leftCandy1.GetComponent<Candy>().isMatched = true;
+                    rightCandy1.GetComponent<Candy>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
+        if (row > 0 && row < board.y_height - 1)
+        {
+            GameObject upCandy1 = board.allCandies[column, row +1];
+            GameObject downCandy1 = board.allCandies[column, row -1];
+            if(upCandy1 !=null & downCandy1 != null)
+            {
+                if (upCandy1.tag == this.gameObject.tag && downCandy1.tag == this.gameObject.tag)
+                {
+                    upCandy1.GetComponent<Candy>().isMatched = true;
+                    downCandy1.GetComponent<Candy>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
     }
 }
